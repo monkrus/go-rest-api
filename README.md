a50daec7c40604d1899874b2bdd4657e7c426e36
Sergei Stadnik <sergeisqa@gmail.com>
2020-03-23 23:45:24 -0700

    Test JSON responsey

--- README.md
+++ README.md
@@ -11,4 +11,15 @@
 
 3. Main function kicks off the API (entry point)
 
-4. Define Article structure
+4. We run `go run main.go` to test the basic functionality of those 3 functions. (Result: "Homepage Endpoint Hit" message )
+
+5. create a fisrs API that returns a JSON response 
+
+6. Define an Article struct, set an Article array
+
+7. Register our function
+
+8. We run `go run main.go`  and return a JSON response under `http://localhost:8000/articles`. Success !
+
+9. 
+
--- main.go
+++ main.go
@@ -1,34 +1,40 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
-/*type Article struct {
-Title string `json:"Title"`
-Desc string `json:"desc"`
-Content string `json:"content"`
-}*/
+type Article struct {
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
 
-/*type Articles[] Article {
-	Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+type Articles []Article
+
+/*	Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
 	Article{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
 }*/
 
-/*func allArticles( w http.ResponseWriter, r *http.Request) {
-fmt.Println("Endpoint Hit: All Articles Endpoint")
-json.NewCoder(w).Encode(articles)
-}*/
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{Title: "Test Title", Desc: "Test Description", Content: "Hello World"},
+	}
 
+	fmt.Println("Endpoint Hit: All articles Endpoint")
+	json.NewEncoder(w).Encode(articles)
+
+}
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Homepage Endpoint Hit")
-	//fmt.Println("Endpoint Hit: homePage")
 }
 
 func handleRequests() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", allArticles)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
 
